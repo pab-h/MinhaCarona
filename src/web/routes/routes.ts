9f07b3ca6a7575145898c32eaf4848b101883c57
pab-h <dev.pab.2020@gmail.com>
2024-10-31 20:42:09 -0300

    feat: docs route

--- src/web/routes/routes.ts
+++ src/web/routes/routes.ts
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 import {handlerError} from "../../middleware/ErrorHandler"
 
+import swaggerUi from "swagger-ui-express";
+import swaggerDocs from "../../docs"
+
 import UsersRoutes from "./users";
 
 const routes = Router();
+
+routes.use(
+    "/docs", 
+    swaggerUi.serve, 
+    swaggerUi.setup(swaggerDocs)
+);
 
 routes.use("/users", UsersRoutes);
 
