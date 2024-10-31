4c45d34040ce84ebb569b7bc86ba8cf5b775dcd8
pab-h <dev.pab.2020@gmail.com>
2024-10-30 21:18:36 -0300

    feat: zod user schemas

---
+++ src/lib/zod.ts
@@ -0,0 +1,18 @@
+import z from "zod";
+
+export const idSchema = z.object({
+    id: z
+        .string({required_error: "id is requested"})
+        .uuid({message: "bad formatted uuid"})
+});
+
+export const createUserSchema = z.object({
+    name: z
+        .string({required_error: "name is requested"}),
+    email: z
+        .string({required_error: "email is requested"})
+        .email({message: "bad formatted email"}),
+    password: z
+        .string({required_error: "password is requested"})
+        .min(6, "password length must be greater than 6")
+});
