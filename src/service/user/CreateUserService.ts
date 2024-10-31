1c3094855f61d7f509a77321786191ec01b7a9cb
pab-h <dev.pab.2020@gmail.com>
2024-10-30 21:19:00 -0300

    feat: create user service

---
+++ src/service/user/CreateUserService.ts
@@ -0,0 +1,45 @@
+import { UserType } from "../../types/UserType";
+import { ArgumentNotValidError } from "../error/ArgumentNotValidError";
+import { UserPrismaRepository } from "../../repository/prisma/UserPrismaRepository";
+import { UserRepository } from "../../repository/interface/UserRepository";
+
+type CreateUserServiceRequest = Omit<
+    UserType, 
+    "id" | "createdAt" | "updatedAt"
+>
+
+type CreateUserServiceResponse = {
+    user: UserType;
+};
+
+export class CreateUserService {
+    private repository: UserRepository;
+    
+    public constructor(repository: UserRepository) {
+        this.repository = repository;
+    }
+
+    public async execute(
+        {
+            email, 
+            password, 
+            name
+        }: CreateUserServiceRequest
+    ): Promise<CreateUserServiceResponse> {
+        const userFound = await this.repository.findByEmail(email);
+
+        if (userFound) {
+            throw new ArgumentNotValidError(
+                "user email already existis"
+            );
+        }
+
+        const user = await this.repository.create({
+            email,
+            password,
+            name
+        });
+
+        return { user };
+    }
+}
