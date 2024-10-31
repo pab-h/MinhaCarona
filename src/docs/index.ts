79539f05b84bb025289c8843eb75751795cb342d
pab-h <dev.pab.2020@gmail.com>
2024-10-31 20:40:54 -0300

    docs: the docs!

---
+++ src/docs/index.ts
@@ -0,0 +1,9 @@
+import basicInfo from "./basicInfo";
+import paths from "./paths";
+import components from "./components";
+
+export default {
+    ... basicInfo,
+    ... components,
+    ... paths,
+}
