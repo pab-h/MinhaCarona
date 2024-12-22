124cffa11603faff1b7918c0535320c39fe26059
pab-h <dev.pab.2020@gmail.com>
2024-12-22 14:04:51 -0300

    fix: cors error

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import 'express-async-errors';
+import cors from "cors";
 import express from 'express';
 
 import { routes } from "./web/routes/routes";
 
 const app = express();
 
+app.use(cors({ origin: "*" }));
 app.use(express.json());
 app.use(routes);
 
