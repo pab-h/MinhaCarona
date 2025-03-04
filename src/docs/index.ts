05857b6b3b9f075f314a1c7aa1bfd62779671a15
pab-h <dev.pab.2020@gmail.com>
2025-03-04 17:16:16 -0300

    cleanup: removendo a dependencia da biblioteca swagger

--- src/docs/index.ts
+++
@@ -1,9 +0,0 @@
-import basicInfo from "./basicInfo";
-import paths from "./paths";
-import components from "./components";
-
-export default {
-    ... basicInfo,
-    ... components,
-    ... paths,
-}
