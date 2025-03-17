39639efc6c58d8c1c0ed56e85a68d023173006ad
pab-h <dev.pab.2020@gmail.com>
2025-03-17 16:53:28 -0300

    feat: repositório das reservas

---
+++ src/repository/prisma/ReservationPrismaRepository.ts
@@ -0,0 +1,39 @@
+import { prisma } from "../../lib/prisma";
+import { ReservationType } from "../../types/ReservationType";
+import { ReservationRepository } from "../interface/ReservationRepository";
+
+export class ReservationPrismaRepository implements ReservationRepository {
+
+    public async create({
+        ownerId,
+        rideId,
+        status
+    }: Omit<ReservationType, "id" | "createdAt" | "updatedAt">): Promise<ReservationType> {
+        return await prisma.reservations.create({
+            data: {
+                owner: { connect: { id: ownerId }},
+                ride: { connect: { id: rideId }},
+                status
+            }
+        })
+    }
+
+    public async findById(id: string): Promise<ReservationType | null> {
+        return await prisma.reservations.findUnique({
+            where: { id }
+        });
+    }
+
+    public async findByOwnerId(ownerId: string): Promise<ReservationType[]> {
+        return await prisma.reservations.findMany({
+            where: { ownerId }
+        });
+    }
+
+    public async findByRideId(rideId: string): Promise<ReservationType[]> {
+        return await prisma.reservations.findMany({
+            where: { rideId }
+        });
+    }
+
+}
